dbacc270da90e47b22bfda8f35fa183fc423cee5
César Vargas Dos Santos <43748428+cesarzxk@users.noreply.github.com>
2021-07-24 09:17:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 
-var json  = Object.assign({}, 'users.json', 'episodes.json', 'posts.json' );
+var json  = Object.assign({}, 'episodes.json', 'posts.json' );
 
-const router = jsonServer.router(json);
+const router = jsonServer.router('users.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
