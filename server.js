2f6c0e1e86cb2ea609ab36cec47e407698e64166
cesarzxk <cesar3d.vargas@gmail.com>
2023-04-22 18:07:46 -0300

    feat(change): :sparkles: concat data

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 
 
+const users = require('./data/users.json')
+const episodes = require('./data/episodes.json')
+const fiftycents = require('./data/fiftycents.json')
 
-const router = jsonServer.router('users.json');
+
+const data = {...users, ...episodes, ...fiftycents}
+
+const json = jsonServer.router(data)
+
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
-server.use(router);
+server.use(json);
+
 server.listen(port);
